1254e4a910af7ecb1d6da5464e2107d0b6365b09
AnujVarma <anuj@prompthire.com>
2020-09-01 14:47:53 -0700

    test

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 export function converter(sentence) {
-  let capitalized = [];
-  let words = sentence.split(" "); //split the sentence into words
-  words.forEach((word) => {
-    let capitalizedWord = word.slice(0, 1).toUpperCase() + word.slice(1); //capitalize the first letter of every word
-    capitalized.push(capitalizedWord);
-  });
-  let converted = capitalized.join(" ");
-  return converted;
+  //   let capitalized = [];
+  //   let words = sentence.split(" "); //split the sentence into words
+  //   words.forEach((word) => {
+  //     let capitalizedWord = word.slice(0, 1).toUpperCase() + word.slice(1); //capitalize the first letter of every word
+  //     capitalized.push(capitalizedWord);
+  //   });
+  //   let converted = capitalized.join(" ");
+  //   return converted;
+  return "OAKLAND";
 }
 
 export class AnujVarma extends HTMLElement {
