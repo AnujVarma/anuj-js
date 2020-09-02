6453f6ea128e9edd228b20519c7be8f4b486b993
AnujVarma <anuj@prompthire.com>
2020-09-02 11:17:43 -0700

    wrapped react in web component standards

---
+++ index.ts
@@ -0,0 +1,47 @@
+// export class AnujVarma extends HTMLElement {
+//   connectedCallback() {
+//     this.innerHTML = `<h1>Explo Site</h1>`;
+//   }
+// }
+
+// customElements.define("anuj-varma", AnujVarma);
+
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import * as retargetEvents from "react-shadow-dom-retarget-events";
+import ReactComponent from "./react-component";
+
+export class AnujV2 extends HTMLElement {
+  static get observedAttributes() {
+    return ["title"];
+  }
+
+  mountPoint: HTMLSpanElement;
+  title: string;
+
+  createCollapsed(title) {
+    return React.createElement(
+      ReactComponent,
+      { title },
+      React.createElement("slot")
+    );
+  }
+
+  connectedCallback() {
+    this.mountPoint = document.createElement("span");
+    const shadowRoot = this.attachShadow({ mode: "open" });
+    shadowRoot.appendChild(this.mountPoint);
+
+    const title = this.getAttribute("title");
+    ReactDOM.render(this.createCollapsed(title), this.mountPoint);
+    retargetEvents(shadowRoot);
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "title") {
+      ReactDOM.render(this.createCollapsed(newValue), this.mountPoint);
+    }
+  }
+}
+
+// window.customElements.define('anuj-v2', AnujV2);
